c9f0a2151d16d75adacd1c2957c5fd4d58509548
Zuhrum <Zuhrum@mail.ru>
2020-04-04 09:02:14 +0700

    Задача №6

---
+++ task6_3.py
@@ -0,0 +1,6 @@
+def my_func(my_string):
+    my_line = str.title(my_string)
+    return print(my_line)
+
+
+my_func('I like nuts')# в описании задачи не сказанно что эту функцию нельзя использовать, она работает в обоих случаях
