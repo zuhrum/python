5352f2e4d5475613321a87b46c862b1c05d8b02b
Zuhrum <Zuhrum@mail.ru>
2020-04-15 19:08:01 +0700

    Задача №4

---
+++ task4_5.py
@@ -0,0 +1,15 @@
+with open('eng.txt') as f:
+    lines = f.readlines()
+
+
+with open('rus.txt', 'w') as f:
+    for line in lines:
+        if '1' in line:
+            line = line.replace('One', 'Один')
+        elif '2' in line:
+            line = line.replace('Two', 'Два')
+        elif '3' in line:
+            line = line.replace('Three', 'Три')
+        elif '4' in line:
+            line = line.replace('Four', 'Четыре')
+        f.write(line)
